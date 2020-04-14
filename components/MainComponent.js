61045319663cbe3e473cdddd2d3840cbe570dcee
Isha <isha7914@gmail.com>
2020-04-15 00:57:12 +0530

    added navigation-cards in menu take to detail page

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,32 +1,20 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
 import DishDetail from "./DishDetailComponent";
-import { View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
-
-  onDishSelect = dishId => {
-    this.setState({ selectedDish: dishId });
-  };
-
   render() {
-    const { selectedDish } = this.state;
     return (
-      <View>
-       <Menu
-         dishes={this.state.dishes}
-         onPress={dishId => this.onDishSelect(dishId)}
-       />
-       <DishDetail dish={this.state.dishes.filter(dish => dish.id === selectedDish)[0]} />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Menu">
+          <Stack.Screen name="Menu" component={Menu} options={{ title: 'Menu' }} />
+          <Stack.Screen name="DishDetail" component={DishDetail} options={{ title: 'Dish Details' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   };
 };

b43aaa5899dfc0671aa25f7795d0433505652add
Isha <isha7914@gmail.com>
2020-04-15 01:31:28 +0530

    used drawer navigation to toggle between home and menu screen

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import DishDetail from "./DishDetailComponent";
+import Home from "./HomeComponent";
+// import DishDetail from "./DishDetailComponent";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
 
 class Main extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Menu">
+        {/* <Stack.Navigator initialRouteName="Menu">
           <Stack.Screen name="Menu" component={Menu} options={{ title: 'Menu' }} />
           <Stack.Screen name="DishDetail" component={DishDetail} options={{ title: 'Dish Details' }} />
-        </Stack.Navigator>
+        </Stack.Navigator> */}
+        <Drawer.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+          <Stack.Screen name="Menu" component={Menu} options={{ title: 'Menu' }} />
+        </Drawer.Navigator>
       </NavigationContainer>
     );
   };

a60755e7da426d261ab7eeb4a9ce5c114c0265ea
Isha <isha7914@gmail.com>
2020-04-15 18:37:58 +0530

    added content in About us component

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,14 +1,60 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, StyleSheet, ScrollView } from "react-native";
+import { ListItem } from "react-native-elements";
+import { Card } from "react-native-elements";
+import { LEADERS } from "../shared/leaders";
+
+const styles = StyleSheet.create({
+  textView: {
+    paddingLeft: 10,
+  }
+});
+
+const History = () => (
+  <Card
+    title="Our History"
+  >
+    <Text>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
+    <Text>{"\n"}The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+  </Card>
+);
 
 class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      leaders: LEADERS
+    };
+  };
+   
   render() {
+    const itemRenderer = ({ item, index }) => (
+      <ListItem
+        key={index}
+        title={<Text style={styles.textView}>{item.name}</Text>}
+        subtitle={<View style={styles.textView}><Text>{item.description}</Text></View>}
+        hideChevron={true}
+        leftAvatar={{ source: require("./images/alberto.png") }}
+      />
+    );
+
+    const leaders = (
+      <Card
+        title="Corporate Leadership"
+      >
+        <FlatList
+          data={this.state.leaders}
+          renderItem={itemRenderer}
+          keyExtractor={item => item.id.toString()}
+        />
+      </Card>
+    );
+
     return (
-      <View>
-        <Text>
-          About Us
-        </Text>
-      </View>
+      <ScrollView>
+        <History />
+        {leaders}
+      </ScrollView>
     );
   };
 }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -9,12 +9,12 @@ class ContactUs extends React.Component {
         <Card
           title="Contact Information"
         >
-          <Text>121, Clear Water Bay Road</Text>
-          <Text>Clear Water Bay, Kowloon</Text>
-          <Text>HONG KONG</Text>
-          <Text>Tel: +852 1234 5678</Text>
-          <Text>Fax: +852 8765 4321</Text>
-          <Text>Email:confusion@food.net</Text>
+          <Text>121, Clear Water Bay Road </Text>
+          <Text>{"\n"}Clear Water Bay, Kowloon</Text>
+          <Text>{"\n"} HONG KONG</Text>
+          <Text>{"\n"} Tel: +852 1234 5678</Text>
+          <Text>{"\n"} Fax: +852 8765 4321</Text>
+          <Text>{"\n"} Email:confusion@food.net</Text>
         </Card>
       </View>
     );

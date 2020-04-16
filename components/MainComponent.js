cc6b8aef0cafd9c6adff10ebd682fd84bea7faf4
Isha <isha7914@gmail.com>
2020-04-16 17:46:30 +0530

    created a custom drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,9 +4,11 @@ import Home from "./HomeComponent";
 import AboutUs from "./AboutComponent";
 import ContactUs from "./ContactComponent";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { Icon } from "react-native-elements";
+import { Image, StyleSheet, ScrollView, View, Text } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
@@ -40,11 +42,29 @@ const ContactStack = () => (
   </Stack.Navigator>
 );
 
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView style={styles.container} forceInset={{ top: "always", horizontal: "never" }}>
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image source={require("./images/logo.png")} style={styles.drawerImage} />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>
+            Ristorante Con Fusion
+          </Text>
+        </View>
+      </View>
+      <DrawerItemList {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
+
 class Main extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Menu">
+        <Drawer.Navigator initialRouteName="Menu" drawerStyle={{ backgroundColor: "#D1C4E9" }} drawerContent={props => <CustomDrawerContentComponent {...props} />}>
           <Drawer.Screen name="Home" component={HomeStack} options={{ drawerIcon: ({ color }) => (
           <Icon name="home" type="font-awesome" size={24} color={color} /> )}} />
           <Drawer.Screen name="About Us" component={AboutStack} options={{ drawerIcon: ({ color }) => (
@@ -60,3 +80,27 @@ class Main extends Component {
 };
 
 export default Main;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  drawerHeader: {
+    backgroundColor: "#512AD8",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row"
+  },
+  drawerHeaderText: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  drawerImage: {
+    margin: 10,
+    width: 80,
+    height: 60
+  }
+});

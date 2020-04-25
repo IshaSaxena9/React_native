801ca40b64e7bd9ead61837a941f2eabbea0cb31
Isha <isha7914@gmail.com>
2020-04-26 04:07:31 +0530

    use react native swipeout to add ios like button for deleting a favourite

--- components/FavouriteComponent.js
+++ components/FavouriteComponent.js
@@ -1,41 +1,53 @@
 import React, { Component } from "react";
-import { View, FlatList, Text} from "react-native";
+import { View, FlatList, Text } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { Loading } from "./LoadingComponent";
+import Swipeout from "react-native-swipeout";
+import { deleteFavourite } from "../redux/ActionCreators";
 
 class Favourites extends Component {
-  render () {
+  render() {
     const { navigate } = this.props.navigation;
-    const { dishes } = this.props;
+    const { dishes, favourites } = this.props;
+
     const renderMenuItem = ({ item, index }) => {
+      const rightButton = [
+        {
+          text: "Delete",
+          type: "delete",
+          onPress: () => this.props.deleteFavourite(item.id)
+        }
+      ];
       return (
-        <ListItem
-        key={index}
-        title={item.name}
-        subtitle={item.description}
-        onPress={() => navigate("DishDetail", { dishId: item.id })}
-        leftAvatar={{ source: { uri: baseUrl + item.image }}}
-        />
+        <Swipeout right={rightButton} autoClose={true}>
+          <ListItem
+            key={index}
+            title={item.name}
+            subtitle={item.description}
+            onPress={() => navigate("DishDetail", { dishId: item.id })}
+            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+          />
+        </Swipeout>
       )
     }
 
-    if(dishes.isLoading) {
+    if (dishes.isLoading) {
       return <Loading />
-    } else if(dishes.errMess) {
+    } else if (dishes.errMess) {
       return (
         <View>
           <Text>{dishes.errMess}</Text>
         </View>
       )
-    } else if(dishes.dishes.length) {
+    } else if (favourites.length) {
       return (
         <FlatList
           data={this.props.dishes.dishes.filter(dish => this.props.favourites.some(item => item === dish.id))}
           renderItem={renderMenuItem}
           keyExtractor={item => item.id.toString()}
-          />
+        />
       )
     } else {
       return (
@@ -45,11 +57,15 @@ class Favourites extends Component {
       )
     }
   }
-};  
+};
 
 const mapStateToProps = state => ({
   dishes: state.dishes,
   favourites: state.favourites
 });
 
-export default connect(mapStateToProps)(Favourites);
+const mapDispatchToProps = dispatch => ({
+  deleteFavourite: dishId => dispatch(deleteFavourite(dishId))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favourites);
